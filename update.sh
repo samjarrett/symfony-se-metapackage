95beeef59eb3928272d1745e3c9ea2c2af1f27a6
Sam Jarrett <sam.jarrett@catchoftheday.com.au>
2017-02-10 12:34:18 +1100

    Add generator

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/sh 
+
+which jq > /dev/null || (echo "jq not present" && exit 1)
+which curl > /dev/null || (echo "curl not present" && exit 1)
+
+LOCAL_TAGS=$(git tag)
+SYMFONY_TAGS=$(curl https://api.github.com/repos/symfony/symfony-standard/tags | jq -r '.[].name')
+#SYMFONY_TAGS="v3.2.3"
+STARTING_DIR=$(PWD)
+
+for TAG in $SYMFONY_TAGS; do
+	echo "Processing tag: $TAG"
+	if git rev-list $TAG.. >/dev/null 2>&1
+	then
+		echo "Tag $TAG already exists!"
+	else
+		git clone . build
+		cd build
+		git checkout --orphan worker-$TAG
+		git rm --cached -r .
+		
+		REQUIRES=$(curl https://api.github.com/repos/symfony/symfony-standard/contents/composer.json\?ref\=$TAG | jq -r '.content' | base64 --decode | jq .require)
+		
+		{
+			cat ../template.json;
+			echo $REQUIRES;
+			echo '}';
+		} | jq --indent 4 '.' > composer.json
+
+		git add composer.json
+		git commit -m "Import symfony/standard-edition@$TAG"
+
+		git tag $TAG
+		
+		git push origin --tags
+		cd $STARTING_DIR
+	fi
+done
